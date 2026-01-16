1c2f52223eb83c37d5d77a81a41cefe078790d67
GDGOC ZCOER <gdgoc.zcoer@zealeducation.com>
2026-01-16 13:37:39 +0530

    Update Solution.java

--- 3_16-01-2026/Java/Solution.java
+++ 3_16-01-2026/Java/Solution.java
@@ -1,3 +1,35 @@
+// https://leetcode.com/problems/insert-interval/
+
 // Yoiu can send pull request of your solution (only correct 1st pull request will get merged)
 
 // Java code
+
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        ArrayList<int[]> ans = new ArrayList<>();
+
+        int i = 0;
+        while(i < intervals.length && intervals[i][1] < newInterval[0]) {
+            ans.add(new int[]{intervals[i][0], intervals[i][1]});
+            i++;
+        }
+        
+        int start = newInterval[0];
+        int end = newInterval[1];
+
+        while(i < intervals.length && intervals[i][0] <= end) {
+            start = Math.min(intervals[i][0], start);
+            end = Math.max(intervals[i][1], end);
+            i++;
+        }
+
+        ans.add(new int[]{start, end});
+
+        while(i < intervals.length) {
+            ans.add(new int[]{intervals[i][0], intervals[i][1]});
+            i++;
+        }
+
+        return  ans.toArray(new int[ans.size()][]);
+    }
+}
