0e050b37a6dc85fde77f8a1f850bf3150bc0c683
Vinayak Jaywant Gawade <164622243+VinayakGawade009@users.noreply.github.com>
2026-01-22 17:15:09 +0530

    Add merge intervals solution in Solution.java
    Implemented the merge intervals algorithm to combine overlapping intervals in a 2D array.

--- 8_22-01-2026/Java/Solution.java
+++ 8_22-01-2026/Java/Solution.java
@@ -1,5 +1,31 @@
-// Problem link: 
+// Problem link: http://leetcode.com/problems/merge-intervals/
 
 // You can send pull request of your solution (only 1st correct pr will get merged)
 
 // java code
+
+class Solution {
+    public int[][] merge(int[][] arr) {
+        int n = arr.length;
+        
+        // Step 1: Sort intervals by start time
+        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
+
+        int index = 0; // Points to the last merged interval in arr
+        
+        // Step 2: Merge intervals in-place
+        for (int i = 1; i < n; i++) {
+            // If intervals overlap, merge them
+            if (arr[i][0] <= arr[index][1]) {
+                arr[index][1] = Math.max(arr[index][1], arr[i][1]);
+            } else {
+                // Move the non-overlapping interval forward
+                index++;
+                arr[index] = arr[i]; // Copy arr[i] to new position
+            }
+        }
+
+        // Step 3: Copy the first (index + 1) elements as the result
+        return Arrays.copyOf(arr, index + 1);
+    }
+}
