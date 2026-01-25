c99d06db251c2ab29e9bc51608f0d508aff80885
Vinayak Jaywant Gawade <164622243+VinayakGawade009@users.noreply.github.com>
2026-01-25 12:58:41 +0530

    Add MyQueue.java file

--- 10_24-01-2026/Java/Solution.java
+++
@@ -1,6 +0,0 @@
-// Problem link: https://leetcode.com/problems/implement-queue-using-stacks/description/
-
-// You can send pull request of your solution (only 1st correct pr will get merged)
-
-// java code
-
