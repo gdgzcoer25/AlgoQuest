db586769557e3a9c7bb751cff46c2974b7a2e10d
GDGOC ZCOER <gdgoc.zcoer@zealeducation.com>
2026-01-26 16:03:27 +0530

    Create Solution.java for reverse Polish notation problem
    Add initial Java solution for evaluating reverse Polish notation.

---
+++ 11_26-01-2026/Java/Solution.java
@@ -0,0 +1,6 @@
+// Problem link: https://leetcode.com/problems/evaluate-reverse-polish-notation/description/
+
+// You can create pull request of your solution (only 1st correct pr will get merged)
+
+// Java code
+
